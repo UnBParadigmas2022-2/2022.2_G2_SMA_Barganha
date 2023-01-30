8684d80702fddcec6058e6cddf7f2f47bf986702
Joao Pedro Moura <joaopedrohm17@gmail.com>
2023-01-30 20:22:29 -0300

    Corrigindo Tela

--- src/BookBuyerGui.java
+++ src/BookBuyerGui.java
@@ -35,7 +35,7 @@ public class BookBuyerGui extends JFrame {
 		myAgent = a;
 		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 386, 480);
+		setBounds(100, 100, 386, 510);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
